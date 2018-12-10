5e36cfb0a41509d6b02dc6892fcd3e9821c52779
chujacky <jacky9407@gmail.com>
2018-12-10 13:51:49 -0800

    Add virtual board for checking winning coditions (eliminate hard code)

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,5 +1,11 @@
 //setup board - view
- 
+
+var board = [
+[0,0,0],
+[0,0,0],
+[0,0,0]
+]
+
 for (var i = 0; i < 3; i++) {
   var div = document.createElement('div');
   div.id = ('row' + i);
@@ -7,7 +13,7 @@ for (var i = 0; i < 3; i++) {
   document.getElementById('container').appendChild(div);
   for (var j = 0; j < 3; j++) {
     var box = document.createElement('button');
-    box.classList.add("button", "col" + j);
+    box.classList.add("button", "col" + j, "row" + i);
     box.style.width = '100px';
     box.style.height = '100px';
     box.style.verticalAlign= 'top';
@@ -29,7 +35,7 @@ for (var i = 0; i < buttons.length; i++){
   buttons[i].onclick = function() {
     this.disabled = true;
     makeMove.call(this, next);
-    checkBoard();
+    checkBoard.call(this, board)
    
   }
 };
@@ -37,8 +43,18 @@ for (var i = 0; i < buttons.length; i++){
 //model
 
 var next = 'X';
-var makeMove = function(button, nextMove) {
+
+var updateBoard = function(button) {
+  var col = Number(button.classList[1].substring(3));
+  var row = Number(button.classList[2].substring(3));
+  board[row][col] = next;
+  console.log(board);
+};
+
+
+var makeMove = function(nextMove) {
   console.log(this);
+  updateBoard(this);
   this.innerHTML = next;
   if (next === 'X') {
     next = 'O';
@@ -47,28 +63,35 @@ var makeMove = function(button, nextMove) {
   }
 };
 
-var checkBoard = function() {
-    checkRows();
-    checkCols();
+var checkBoard = function(board) {
+    if (checkRow.call(this,board) || checkCols.call(this,board) || checkDiagonals()) {
+      for (var i = 0; i < buttons.length; i++){
+        buttons[i].disabled = true; 
+      }
+      document.getElementById('result').innerHTML = this.innerHTML + ' wins!'
+    }
 };
 
-var checkRows = function() {
-  for (var i = 0; i < 3; i++){
-    var rows = document.querySelectorAll('#row' + i + ' button');
-    if (rows[0].innerHTML!== "" && (rows[0].innerHTML === rows[1].innerHTML) && (rows[0].innerHTML === rows[2].innerHTML)) {
-      document.getElementById('result').innerHTML = rows[0].innerHTML + " Wins!";
+var checkRow = function(board) {
+  var row = Number(this.classList[2].substring(3));
+  for (var i = 0; i < board[row].length; i++) {
+    if (board[row][i] !== this.innerHTML){
+      return false;
     }
-  }
+  };
+  return true;
 };
 
-var checkCols = function() {
-  for (var i = 0; i < 3; i++){
-    var cols = document.querySelectorAll(".col" + i);
-    if (cols[0].innerHTML!== "" && (cols[0].innerHTML === cols[1].innerHTML) && (cols[0].innerHTML === cols[2].innerHTML)) {
-      document.getElementById('result').innerHTML = cols[0].innerHTML + " Wins!";
+var checkCols = function(board) {
+  var col = Number(this.classList[1].substring(3));
+  for (var i = 0; i < board.length; i++) {
+    if (board[i][col] !== this.innerHTML){
+      return false;
     }
-  }
-}
+  };
+  return true;
+};
+
 
 var checkDiagonals = function() {
   
