9478f0ea3166b178b8ad476a96596b0d462928f1
chujacky <jacky9407@gmail.com>
2018-12-11 15:24:06 -0800

    Created fuction to get each individual object

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,15 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const multer = require('multer'); 
 const app = express();
+const flatten = require('./client/app')
 // const upload = multer();
 
 app.use(express.static('client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/submit', (req, res) => {
-  console.log(req.body)
-  res.status(200).send(req.body);
+app.post('/', (req, res) => {
+  var data = req.body;
+  console.log('----------', data);
+  data = data.json.split("\r\n").join("");
+  // var result = flatten.flatten(data);
+  console.log(data);
+  res.status(200).send(data);
 })
 
 app.listen(3000, () => {console.log("CSV Report Generator is listening")});
