fca236957aa0dd60dbd6da03938003920132bf05
chujacky <jacky9407@gmail.com>
2018-12-11 12:26:23 -0800

    Setup server and connect form to server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const multer = require('multer'); 
+const app = express();
+// const upload = multer();
 
+app.use(express.static('client'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/submit', (req, res) => {
+  console.log(req.body)
+  res.status(200).send(req.body);
+})
+
+app.listen(3000, () => {console.log("CSV Report Generator is listening")});
 
